cd6ace2d8f2bab29b960244715ee1e583983937f
xl3ehindTim <timbrouwers1@gmail.com>
2024-03-11 11:15:37 +0100

    Extract json from output

--- app/summarization.py
+++ app/summarization.py
@@ -1,21 +1,28 @@
 import os
+import json
+
 import logging
 import torch
 
 from transformers import pipeline
 
+
+def extract_json(s):
+    s = s[next(idx for idx, c in enumerate(s) if c in "{[]}"):]
+    try:
+        return json.loads(s)
+    except json.JSONDecodeError as e:
+        return json.loads(s[:e.pos])
+
+
 model_name = "mistralai/Mistral-7B-Instruct-v0.2"
 subject_extraction = pipeline(
-    "text-summarization", model=model_name, device=0 if torch.cuda.is_available() else -1
+    "text-summarization", model=model_name, device_map="auto"
 )
 
 conversation = "Can you summarize this conversation in one sentence, highlighting the key point or topic discussed? Here is the conversation: Whats ur band called? its called 1 idiot, 1 bass guitar"
 
-with torch.cuda.amp.autocast_enabled() if torch.cuda.is_available() else torch.no_grad():
-    output = subject_extraction(conversation, max_length=100, truncation=True)
+output = subject_extraction(conversation, max_length=250, truncation=True)
+parsed = extract_json(output[0]["summary_text"])
 
-print("output", output)
-
-print("-----------------------------------")
-
-print(output[0]["summary_text"])
+print(parsed)
