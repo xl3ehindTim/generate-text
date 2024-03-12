a0820fae8eb9cc739b5d1b240ab246fb99260a69
xl3ehindTim <timbrouwers1@gmail.com>
2024-03-12 13:07:47 +0100

    Update usage

--- app/app.py
+++ app/app.py
@@ -1,79 +1,45 @@
 import os
 import logging
 import torch
+import json
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
 from flask import Flask, request, jsonify
 
-# Load the model and tokenizer with GPU support (if available)
-model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-device = "cuda" if torch.cuda.is_available() else "cpu" 
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16) # check if float32 runs
-model.to(device)
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+def extract_json(s):
+  s = s[next(idx, c in enumerate(s) if c in "{["):]
+  try:
+    return json.loads(s)
+  except json.JSONDecodeError as e:
+    return json.loads(s[:e.pos])
 
-# Initialize Flask application
+
+model         = "mistralai/Mistral-7B-Instruct-v0.2"
+generator     = pipeline("text-generation", model=model, device_map="auto")
+instruction   = "My goal is to understand what is being talked about in a conversion. I want to know what the subjects of the conversation are and get some keywords from the conversation. Format the data as follows: [{subject: "", keywords: "" }, {subject: "", keywords: "" }] The conversation is as follows: "
+max_length    = 400
+
 app = Flask(__name__)
 
-
-@app.route("/extract", methods=["POST"])
-def extract_topics(text):
-  text = request.json.get("prompt")
-
-  prompt = f"What are the main topics discussed in the following conversation, please return a JSON format and nothing more: {text}"
-  inputs = tokenizer(prompt, return_tensors=device)
-  output = model.generate(**inputs)
-  decoded_text = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
-
-  # Identify potential JSON start and end
-  start_index = decoded_text.find('{')
-  end_index = decoded_text.rfind('}') 
-
-  # Extract potential JSON content (if valid indices found)
-  if start_index >= 0 and end_index > start_index:
-    json_content = decoded_text[start_index:end_index+1]
-  else:
-    json_content = None  # No valid JSON found
-
-  # Try parsing and handling potential errors
-  try:
-    if json_content:
-      data = json.loads(json_content)
-      topics = [{"Topic": topic["Topic"]} for topic in data["Topics"]]  
-      return jsonify.dumps({"Topics": topics})
-    else:
-      return json.dumps({"Error": "No valid JSON found"})
-  except json.JSONDecodeError:
-    return jsonify.dumps({"Error": "Invalid JSON format"})
+conversation = "[“Guys is it bad if you drink soap”, “I washed my bottle out with fairy liquid but judging by the taste of my vimto theres still some left in there”, “Probably just gonna start breathing bubbles”, “Whenever I’ve been home alone I’ve ended up drinking it by accident tbh. Don’t taste too bad tbf”, “There was no Three cliffs bbq”, “​​This might be a shot in the dark, but as I know these, this band is a Reading fans band and we’re trying to keep our club alive. We don’t know whether the owner will pay the players, wages or points deductions or if we will survive by the end of the season, so if you live near or around Reading and you’re thinking about going to a game, but you don’t know whether to do it or not please we’re trying to get we’ve got Five or six games left at home and we need as many people as we can for those home games so that we can try and help the players get money and the owner and and the manager gets what he deserves . No pressure. Just looking to see if some people want to come and thank you.” ]"
+output = generator(instruction + conversation, max_length=max_length)
+print(output[0]["generated_text"])
+parsed_output = extract_json(output[0]["generated_text"])
+print(parsed_output)
 
 
+@app.route("/subjects", methods=["POST"])
+def get_subjects():
+  conversation = request.json.get("conversation")
+  if not conversation:
+    return "Error: No conversation parameter supplied in the request body", 400
 
-@app.route("/generate", methods=["POST"])
-def generate_text():
-  # Get the prompt from the request data
-  prompt = request.json.get("prompt")
+  output = generator(instruction + conversation, max_length=max_length)
+  print(output[0]["generated_text"])
+  parsed_output = extract_json(output[0]["generated_text"])
 
-  # Check if prompt is provided
-  if not prompt:
-    return "Error: Please provide a prompt in the 'prompt' field of the request body.", 400
-
-  inputs = tokenizer(prompt, return_tensors=device)
-  output = model.generate(**inputs)
-  decoded_text = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
-
-  # Prepare the input
-  input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
-
-  # Generate text with custom maximum length
-  max_length = 500
-  output = model.generate(input_ids, max_new_tokens)
-
-  # Decode the generated sequence
-  generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-  # Return the generated text
-  return jsonify({"generated_text": generated_text})
+  return jsonify(parsed_output)
 
 
 if __name__ == "__main__":
